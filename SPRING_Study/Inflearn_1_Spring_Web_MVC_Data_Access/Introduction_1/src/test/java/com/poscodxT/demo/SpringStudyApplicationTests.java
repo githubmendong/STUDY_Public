e631f56c15afa6af74cfc7f49dc824c93300ce90
shin-dongheon <tlsejdgjs@naver.com>
2023-08-31 11:05:23 +0900

    .

---
+++ SPRING_Study/Inflearn_1_Spring_Web_MVC_Data_Access/Introduction_1/src/test/java/com/poscodxT/demo/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscodxT.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
